12a457bed373a1029ddafba5ecaf8563c37eb7b8
Pedram <pedramshirin@hotmail.com>
2017-02-02 23:23:31 +0100

    Some stuff
    Initial fiddling with methods.

--- SourceCode/src/Parking.java
+++ SourceCode/src/Parking.java
@@ -1,5 +1,18 @@
 package src;
 
 public class Parking {
+	int sensor1;
+	int sensor2;
 
+	/*This method queries the two ultrasound sensors at least 5 times and filters 
+	 * the noise in their results and returns the distance to the nearest object in the right hand side.
+	 *  If one sensor is detected to continuously return very noisy output, it should be completely disregarded.  
+	 *  You can use averaging or any other statistical method to 
+	 * filter the noise from the signals received from the ultrasound sensors. */
+	public int isEmpty(int objectDistance){
+		
+		return objectDistance;
+	}
+	
+	
 }
